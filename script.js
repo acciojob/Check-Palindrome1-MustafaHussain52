01b670cfee3b1303fcd76d81892c3826f71d87f5
Mustafa Hussain <118181312+MustafaHussain52@users.noreply.github.com>
2023-06-13 19:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,24 @@
 
 function palindrome(str){
 
+ // Convert the string to lowercase and remove non-alphanumeric characters
+  const alphanumericString = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  // Initialize pointers
+  let left = 0;
+  let right = alphanumericString.length - 1;
+
+  while (left < right) {
+    // Compare characters at the two pointers
+    if (alphanumericString[left] !== alphanumericString[right]) {
+      return false;
+    }
+
+    // Move pointers
+    left++;
+    right--;
+  }
+
+  return true;
 }
 module.exports = palindrome
